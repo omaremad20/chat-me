ee0a77c7bea7f017c198e15a5804ea6c61b512d8
omaremad20 <oe747745@gmail.com>
2026-02-28 19:13:10 +0200

    fix:supabase error

--- app/_lib/supabase.ts
+++ app/_lib/supabase.ts
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY!,
+  "https://dwqthdwtdxvujqynxnyk.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3cXRoZHd0ZHh2dWpxeW54bnlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2ODMxMjMsImV4cCI6MjA4NzI1OTEyM30.xqzhEnXaCeN_ZYnTHs-Z04xWafJ8uB_w5ftcDT8kaBA",
 );
-
-console.log(  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY!,)
